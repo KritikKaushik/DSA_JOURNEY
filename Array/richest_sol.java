5b29c10a85d9c3559483294320a4d85faa3aa70c
Kritik <kritikkaushik2004@gmail.com>
2026-06-13 13:28:18 +0530

    Create richest_sol.java

---
+++ Array/richest_sol.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        
+        int res=0;
+        for(int[] acc : accounts){
+            int sum=0;
+            for(int money : acc){
+                sum+=money;
+                res=Math.max(res,sum);
+            }
+        }
+        return res;
+    }
+}
