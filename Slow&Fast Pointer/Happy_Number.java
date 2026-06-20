32b7240c36099e565d4e663b1a66bf9f8d0894d4
Kritik <kritikkaushik2004@gmail.com>
2026-06-20 18:11:11 +0530

    Create Happy_Number.java

---
+++ Slow&Fast Pointer/Happy_Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static int sum(int num)
+    {
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            num=num/10;
+            sum=sum+(digit*digit);
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        while(fast!=1){
+            slow=sum(slow);
+            fast=sum(fast);
+            fast=sum(fast);
+
+            if(slow==fast && slow!=1){
+                return false;
+            }
+        }
+        return true;       
+    }
+}
