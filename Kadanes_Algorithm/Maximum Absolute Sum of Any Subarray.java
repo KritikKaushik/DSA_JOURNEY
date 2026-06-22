dfa4e52ca41fc78b7c42b5756bbbf6a16fb4fc18
Kritik <kritikkaushik2004@gmail.com>
2026-06-22 22:28:38 +0530

    Create  Maximum Absolute Sum of Any Subarray.java

---
+++ Kadanes_Algorithm/Maximum Absolute Sum of Any Subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int i=0;
+        int sum=nums[i];
+        int nsum=nums[i];
+        int ans=Math.abs(nums[i]);
+        
+
+        for(i=1;i<nums.length;i++){
+            sum=Math.max(nums[i],sum+nums[i]);
+
+            nsum=Math.min(nums[i],nsum+nums[i]);
+
+            ans=Math.max(ans,Math.max(sum,Math.abs(nsum)));
+
+        }
+        return ans;
+    }
+}
