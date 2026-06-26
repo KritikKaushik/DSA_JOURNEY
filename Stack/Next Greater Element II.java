c0a60e461983e69147a444c38b73db349d379a66
Kritik <kritikkaushik2004@gmail.com>
2026-06-26 22:22:39 +0530

    Create Next Greater Element II.java

---
+++ Stack/Next Greater Element II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int j = n - 2; j >= 0; j--) {
+            st.push(nums[j]);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.peek();
+            }
+
+            st.push(nums[i]);
+        }
+        return ans;
+
+    }
+}
