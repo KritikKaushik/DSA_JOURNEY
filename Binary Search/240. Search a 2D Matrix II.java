7bc12af27ef08472239813566eade9002ced5c98
Kritik <kritikkaushik2004@gmail.com>
2026-06-30 22:11:59 +0530

    Create 240. Search a 2D Matrix II.java

---
+++ Binary Search/240. Search a 2D Matrix II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int r = row-1;
+        int c = 0;
+
+        while (r >= 0 && c < col) {
+
+            if (matrix[r][c] == target) {
+                return true;
+            } else if (matrix[r][c] < target) {
+                c++;
+
+            } else {
+                r--;
+            }
+        }
+        return false;
+
+    }
+}
