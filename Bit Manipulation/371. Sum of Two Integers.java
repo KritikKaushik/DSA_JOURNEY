70e9872698db99a706bcf002828ff1cfcaafca76
Kritik <kritikkaushik2004@gmail.com>
2026-07-01 22:22:25 +0530

    Create 371. Sum of Two Integers.java

---
+++ Bit Manipulation/371. Sum of Two Integers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while(b!=0){
+            int temp=(a&b)<<1;// it will store carry and left shift it
+            a=a^b;// 100^010=110
+            b=temp;//
+        }
+        return a;
+    }
+}
