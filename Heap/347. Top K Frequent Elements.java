3a2a48b716dc9f81310c9770184ac86942f42d1b
Kritik <kritikkaushik2004@gmail.com>
2026-07-01 22:24:17 +0530

    Create 347. Top K Frequent Elements.java

---
+++ Heap/347. Top K Frequent Elements.java
@@ -0,0 +1,57 @@
+class Pair {
+    int num;
+    int freq;
+
+    Pair(int num, int freq) {
+        this.num = num;
+        this.freq = freq;
+    }
+}
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+                (a, b) -> {
+                    if (a.freq != b.freq) {
+                        return Integer.compare(a.freq, b.freq);
+                    }
+                    return Integer.compare(a.num, b.num);
+                });
+
+        for (int n : map.keySet()) {
+            int number = n;
+            int frequency = map.get(n);
+            Pair m = new Pair(number, frequency);
+            if (pq.size() < k) {
+                pq.add(m);
+                continue;
+            }
+            Pair temp = pq.peek();
+            if (pq.size() == k) {
+
+                if (m.freq > temp.freq) {
+                    pq.poll();
+                    pq.add(m);
+                }
+            }
+            if (m.freq < temp.freq) {
+                continue;
+            }
+        }
+        int[] res = new int[k];
+        int index = 0;
+        while (!pq.isEmpty()) {
+            Pair ans = pq.poll();
+            res[index] = ans.num;
+            index++;
+        }
+
+        return res;
+
+    }
+}
