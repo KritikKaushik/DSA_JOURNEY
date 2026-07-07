62a70fac33c93f9af2c4d6cfdf5500fef97068d0
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-07-07 21:28:04 +0530

    Create 70. Climbing Stairs.java

---
+++ Dynamic Programming/70. Climbing Stairs.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    
+
+    public int climbStairs(int n) {
+        
+        int[] dp=new int[3];
+        if(n<=2){
+            return n;
+        }
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+            dp[2]=dp[0]+dp[1];
+            
+        }
+
+        return dp[2];
+    }
+}
