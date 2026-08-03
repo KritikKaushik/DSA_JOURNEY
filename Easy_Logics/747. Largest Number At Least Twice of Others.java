1ff319b941f6ec4bfbd2dc5d85694b39419704f4
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-08-03 21:15:43 +0530

    Implement solution for dominant index problem

---
+++ Easy_Logics/747. Largest Number At Least Twice of Others.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max1=-1;
+        int max2=-1;
+        int maxIndex=-1;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max1){
+                max2=max1;
+                max1=nums[i];
+                maxIndex=i;
+            }else if(nums[i]>max2){
+                max2=nums[i];
+            }
+        }
+        if(max1>=(2*max2)){
+            return maxIndex;
+        }
+        return -1;
+    }
+}
