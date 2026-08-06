3daaea06fcc8312d2cf141200c79b91e1d692b3d
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-08-06 23:45:17 +0530

    Create 3345. Smallest Divisible Digit Product I.java

---
+++ Easy_Logics/3345. Smallest Divisible Digit Product I.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        int num=n;
+
+        while(true){
+            int prod=1;
+            n=num;
+            while(n>0){
+                prod*=(n%10);
+                n/=10;
+            }
+            if(prod%t==0){
+                break;
+            }else{
+                ++num;
+            }
+        }
+        return num;
+    }
+}
